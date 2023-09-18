19b64160e57c68f2a6f72b748a11704867e1191d
marcasty <markacastellano2@gmail.com>
2023-09-18 00:44:08 -0400

    coco dataset class, crawl function

---
+++ data.py
@@ -0,0 +1,153 @@
+from datetime import datetime
+import os
+import numpy as np
+import pandas as pd
+import itertools
+import tqdm
+from PIL import Image
+import matplotlib.pyplot as plt
+# Machine Learning Core
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision import transforms, models
+from torch.utils.data import Dataset, DataLoader, Subset
+import json
+import argparse
+from icrawler.builtin import GoogleImageCrawler
+
+class COCO_MetaData:
+    """ stores meta data on images in COCO format"""
+    def __init__(self, json_file_path = None):
+        # create a new json if none are supplied
+        if json_file_path is None:
+            self.coco = { 
+                "info": {
+                    "description": "MealSegmentation Dataset from Google Images",
+                    "version": "1.0",
+                    "year": 2023,
+                    "contributor": "marcasty, pranav270-create",
+                    "date_created": "2023-09-15"
+                },
+                "categories":[],
+                "images": [],
+                "annotations": []
+                }
+        
+        # create coco object from json file
+        else: 
+            with open(json_file_path, 'r') as f:
+                self.coco = json.load(f)
+
+        self.num_categories = len(self.coco["categories"])
+        self.num_images = len(self.coco["images"])
+
+    def get_num_categories(self): return self.num_categories
+
+    def add_categories(self, new_foods):
+        # create a set of current foods for efficient comparisons
+        old_food_set = set(self.coco["categories"])
+        if isinstance(new_foods, list):
+            new_foods_unique = [item for item in new_foods if item not in old_food_set]
+            self.coco["categories"].extend(new_foods_unique)
+        else:
+            if new_foods not in old_food_set:
+                self.coco["categories"].append(new_foods)
+        self.num_categories = len(self.coco["categories"])
+
+    # returns number of images 
+    def get_num_images(self): return self.num_images
+
+    # adds image meta data to json
+    def add_image_data(self, filename, search_query, width, height):
+        image_data = {
+            "id": self.num_images + 1,
+            "Google Search Query": search_query,
+            "width": width,
+            "height": height,
+            "filename": filename,
+            "date captured": datetime.now().strftime('%Y-%m-%d')
+        }
+        self.coco["images"].append(image_data)
+        self.num_images += 1
+
+    def export_coco(self, replace = False):
+        # do you wish to replace the existing json file?
+        if replace is False:
+            current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            file_name = f"data_{current_datetime}.json"
+        else: filename = "data.json"
+
+        with open(file_name, "w") as json_file:
+            json.dump(self.coco, json_file)
+
+def crawl_google_images(metadata, new_foods, save_dir, quantity=10):
+    id_counter = metadata.get_num_images() + 1
+    
+    # set up crawler
+    google_Crawler = GoogleImageCrawler(storage = {'root_dir': save_dir})
+
+    # save a list of images already in the directory
+    old_images = os.listdir(save_dir)
+
+    # add truly new foods to list of categories 
+    num_cat_old = metadata.get_num_categories
+    metadata.add_categories(new_foods)
+    num_cat_new = metadata.get_num_categories
+
+    # execute the crawl across the entire list of new foods 
+    for i in range(num_cat_old+1, num_cat_new):
+        google_Crawler.crawl(keyword=metadata["categories"][i], max_num=quantity)
+
+        # list of newly scraped filenames
+        new_images = list(set(os.listdir(save_dir)) - set(old_images))
+        
+        # make sure the query does not contain characters that are forbidden in filenames
+        forbidden_chars = ' <>:"/\\|?*_'  # Include underscore for later convenience
+        clean_filename = ''.join(['-' if char in forbidden_chars else char for char in query])
+        
+        for idx, image_name in enumerate(new_images):
+            # save old path
+            image_path = os.path.join(save_dir, image_name)
+            
+            # get image width/height
+            image = Image.open(image_path)
+            width, height, = image.size 
+
+            # form new name based on query and index
+            image_num = f"{idx+1:05}"
+            new_image_name = f'{clean_filename}_{image_num}.jpg'  # if image_name.endswith('.jpg') else f'{query}_{idx+1}.png'
+            
+            # add meta data to JSON file
+            metadata.add_image_data(new_image_name, search_query=metadata["categories"][i], width, height)
+
+            # create new path
+            new_image_path = os.path.join(save_dir, new_image_name)
+            # rename image to new path
+            os.rename(image_path, new_image_path)
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Defines a metadata object in COCO format and scrapes Google for images of food.")
+    parser.add_argument("img_dir", type=str, help="Directory to save images")
+    parser.add_argument("--metadata_json", type=str, help="JSON file containing metadata in COCO format")
+    parser.add_argument("new_foods_text", type=str, help="text file containing new foods to scrape")
+    parser.add_argument("--num_examples", type=int, help="number of examples per food you wish to scrape")
+    parser.add_argument("--query", type=str, help="a single query to scrape")
+    return parser.parse_args()
+
+# must include:
+#   save directory
+#   textfile containing prospective new foods
+if __name__ == '__main__':
+    args = parse_arguments()
+
+     # either opens supplied json or creates new coco file
+    
+    metadata = COCO_MetaData(args.metadata_json)
+    with open(args.new_foods_text, "r") as f:
+        new_foods = f.readlines()
+
+    crawl_google_images(metadata, new_foods, args.img_dir, args.num_examples)
+
+    # export metadata to json file
+    metadata.export_coco()
