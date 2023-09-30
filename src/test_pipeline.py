8f2b71c5bc52531ab9e7cd09287acaa2a94a1ed4
marcasty <markacastellano2@gmail.com>
2023-09-30 17:05:23 -0400

    adding annotations to visualize dino/sam results

--- src/test_pipeline.py
+++ src/test_pipeline.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import cv2
 import numpy as np
-from FoodMetadatametadata.coco import FoodMetadata
-
+from FoodMetadataCOCO import FoodMetadata
+import supervision as sv
 
 # Let us make a function that adds masks and boxes and classes to the existing dataset
 def add_masks_and_labels(img_dir, data_file, box_thresh=0.35, text_thresh=0.25):
@@ -12,8 +13,8 @@ def add_masks_and_labels(img_dir, data_file, box_thresh=0.35, text_thresh=0.25):
     for id, annot in metadata['images'].items():
         SOURCE_IMAGE_PATH = f"{img_dir}{annot['filename']}"
 
-        image_bgr = cv2.imread(SOURCE_IMAGE_PATH)
-        image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+        image = cv2.imread(SOURCE_IMAGE_PATH)
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         id = metadata.coco["annotations"][id]['id']
 
@@ -32,7 +33,23 @@ def add_masks_and_labels(img_dir, data_file, box_thresh=0.35, text_thresh=0.25):
         mask = metadata.metadata.coco["annotations"][id]["masks"]
         scores = metadata.metadata.coco["annotations"][id]["mask_confidence"]
 
-        plt.figure()
-        plt.imshow(image_rgb)
-        plt.annotate(blip2, xy=(2, 1), xytext=(3, 1.5))
-        plt.annotate(spacy, xy=(2, 1), xytext=(3, 1.5))
+        fig, ax = plt.subplots()
+        ax.imshow(image_rgb)
+        ax.annotate(blip2, xy=(2, 1), xytext=(3, 1.5))
+        ax.annotate(spacy, xy=(2, 1), xytext=(3, 1.5))
+        
+        # annotate image with detections
+        box_annotator = sv.BoxAnnotator()
+        labels = [f"{classes[class_ids[i]]} {box_confidence[i]:0.2f}" for i in range(num_objects)]
+        annotated_frame = box_annotator.annotate(scene=image.copy(), detections=detections, labels=labels)
+        
+        sv.plot_image(annotated_frame, (16, 16))
+
+
+        # Create a Rectangle patch
+        rect = patches.Rectangle((50, 100), 40, 30, linewidth=1, edgecolor='r', facecolor='none')
+
+        # Add the patch to the Axes
+        ax.add_patch(rect)
+
+        plt.show()
