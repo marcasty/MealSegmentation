ecebb39adf1b81c7d175e7e0cad03616a8201821
marcasty <markacastellano2@gmail.com>
2023-10-01 15:38:10 -0400

    save boxed image

--- src/test_pipeline.py
+++ src/test_pipeline.py
@@ -32,24 +32,57 @@ def add_masks_and_labels(img_dir, data_file, box_thresh=0.35, text_thresh=0.25):
         # SAM STUFF
         mask = metadata.metadata.coco["annotations"][id]["masks"]
         scores = metadata.metadata.coco["annotations"][id]["mask_confidence"]
+        # save_filename = 
 
-        fig, ax = plt.subplots()
-        ax.imshow(image_rgb)
-        ax.annotate(blip2, xy=(2, 1), xytext=(3, 1.5))
-        ax.annotate(spacy, xy=(2, 1), xytext=(3, 1.5))
-        
-        # annotate image with detections
-        box_annotator = sv.BoxAnnotator()
-        labels = [f"{classes[class_ids[i]]} {box_confidence[i]:0.2f}" for i in range(num_objects)]
-        annotated_frame = box_annotator.annotate(scene=image.copy(), detections=detections, labels=labels)
-        
-        sv.plot_image(annotated_frame, (16, 16))
+        plot_sample(image_rgb, boxes, box_confidence, classes, class_ids, save_filename)  
 
 
-        # Create a Rectangle patch
-        rect = patches.Rectangle((50, 100), 40, 30, linewidth=1, edgecolor='r', facecolor='none')
 
-        # Add the patch to the Axes
-        ax.add_patch(rect)
+import matplotlib.patches as patches
+import matplotlib.patheffects as patheffects
+import matplotlib.pyplot as plt
 
-        plt.show()
+
+def img_show(img, ax = None, figsize=(7,11)):
+  fig, ax = plt.subplots(figsize=figsize)
+  #image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+  ax.xaxis.tick_top()
+  ax.imshow(img)
+  return ax
+
+def get_bb(box):
+  x = box[0]
+  y = box[1]
+  w = box[2] - box[0]
+  h = box[3] - box[1]
+  return [x,y,w,h]
+
+def draw_box(img, ax, bb):
+  patch = ax.add_patch(patches.Rectangle((bb[0],bb[1]), bb[2], bb[3], fill=False, edgecolor='red', lw=2))
+
+def draw_outline(obj):
+  obj.set_path_effects([patheffects.Stroke(linewidth=4,  foreground='black'), patheffects.Normal()])
+
+def draw_text(ax, bb, txt, disp):
+  text = ax.text(bb[0],(bb[1]-disp),txt,verticalalignment='top',color='white',fontsize=10,weight='bold')
+  draw_outline(text)
+
+def plot_sample(img, bboxes, box_confidence, classes, class_ids, filename, ax=None, figsize=(7,11)):
+    ax = img_show(img, ax=ax, figsize=figsize)
+    for i in range(len(bboxes)):
+        bb = get_bb(bboxes[i])
+        draw_box(img,ax,bb)
+        label = f'{str(classes[class_ids[i]])}, {box_confidence[class_ids[i]]:.2f}'
+        draw_text(ax, bb, label, img.shape[0]*0.05)
+    plt.savefig(filename)
+
+def multiplot(image, boxes, classes, class_ids, figsize=(20, 5)):
+    num_objects = len(boxes)
+    fig, ax = plt.subplots(1, num_objects + 1, figsize=figsize)
+    plt.subplots_adjust(wspace=0.1, hspace=0)
+    fig.tight_layout()
+    plot_sample(image, boxes, classes, class_ids, ax=ax[0])
+    for i, axs in enumerate(ax):
+        img = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        label = f'{str(classes[class_ids[i]])}, {box_confidence[class_ids[i]]:.2f}'
+        plot_sample(img, [boxes[i]], classes, [class_ids[i]], ax=ax[i + 1])
