0e9336090b5ed021b954b3691a1ef0bbebcce0fc
root <marcasty>
2023-10-13 14:46:28 +0000

    flake8 and black

--- src/caption_to_keyword.py
+++ src/caption_to_keyword.py
@@ -1,45 +1,48 @@
 from string import punctuation
 
+
 def spacy_setup(specific_model):
     import spacy
+
     model = spacy.load(specific_model)
     return model
 
-def run_spacy(text: str, **kwargs) -> set:
+
+def run_spacy(text: str, **kwargs) -> list:
     """given text, produce hot words"""
 
-    if 'spacy' in kwargs:
-        spacy = kwargs['spacy']
+    if "spacy" in kwargs:
+        spacy = kwargs["spacy"]
     else:
         raise AssertionError("No Spacy Model Provided")
 
     result = []
-    pos_tag = ['PROPN', 'NOUN']
+    pos_tag = ["PROPN", "NOUN"]
     doc = spacy(text.lower())
     for token in doc:
-        if (token.text in spacy.Defaults.stop_words or token.text in punctuation):
+        if token.text in spacy.Defaults.stop_words or token.text in punctuation:
             continue
-        elif (token.pos_ in pos_tag):
+        elif token.pos_ in pos_tag:
             result.append(token.text)
-    return set(result)
+    return list(set(result))
+
 
 def get_keywords(metadata, **kwargs):
     if "model" not in kwargs:
-        raise AssertionError("Must give a model to extract keywords")
-    
+        raise AssertionError("Must Give a Model to Extract Keywords")
+
     if "testing" in kwargs:
         testing = kwargs["testing"]
     else:
         testing = False
-    
+
     if "specific_model" in kwargs:
         spacy = spacy_setup(kwargs["specific_model"])
     else:
-        raise AssertionError("Must specify model details to extract keywords")
-    
+        raise AssertionError("Must Specify Model Details to Extract Keywords")
+
     count = 0
     for cat_id, cat in metadata.cats.items():
-
         count += 1
         if count > 3 and testing is True:
             return metadata
@@ -49,8 +52,7 @@ def get_keywords(metadata, **kwargs):
 
         for img_id in img_ids:
             for ann in metadata.imgToAnns[img_id]:
-                if ann['category_id'] == cat_id: 
+                if ann["category_id"] == cat_id:
                     keywords = run_spacy(ann["caption"], spacy)
-                    ann_id = metadata.add_annotation(img_id, cat_id)
-                    metadata.add_text_annot(ann_id, img_id, "keywords", keywords)
+                    metadata.add_text_annot(ann["id"], img_id, "keywords", keywords)
     return metadata
