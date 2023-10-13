1964abe117e94b30160f7c06a1993dee1556f7c3
marcasty <markacastellano2@gmail.com>
2023-10-13 00:44:18 -0400

    flake 8 pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,5 +1,5 @@
 import hydra
-from omegaconf import DictConfig, OmegaConf
+from omegaconf import DictConfig
 from FoodMetadataCOCO import FoodMetadata
 from image_to_caption import get_captions
 import torch
@@ -10,22 +10,25 @@ HOME = os.path.expanduser("~")
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 HOME = '/tmp'
 
+
 @hydra.main(version_base=None, config_path="../conf", config_name="config")
-def main(cfg : DictConfig) -> None:
+def main(cfg: DictConfig) -> None:
     if not os.path.exists(cfg.path.save_dir):
         os.makedirs(cfg.path.save_dir)
 
-    metadata = FoodMetadata(cfg.file.metadata, pred = True)
+    metadata = FoodMetadata(cfg.file.metadata, pred=True)
 
-    if cfg.stage.image_to_caption.is_component == True:
-        metadata = get_captions(metadata, model= cfg.stage.image_to_caption.model,
-                                image_dir = cfg.path.images,
-                                testing = cfg.var.testing,
-                                model_variation = cfg.stage.image_to_caption.model_variation
-                                )
-        print(metadata.anns[1])        
-        
+    if cfg.stage.image_to_caption.is_component:
+        metadata = get_captions(
+            metadata,
+            model=cfg.stage.image_to_caption.model,
+            image_dir=cfg.path.images,
+            testing=cfg.var.testing,
+            specific_model=cfg.stage.image_to_caption.specific_model
+            )
+        print(metadata.anns[1])
         # do a check
+
 
 if __name__ == "__main__":
     main()
